4769710a15d2b76d5ffa8a95e4dde1dc4bae3afa
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-04-26 16:39:54 +0100

    initial commit of controller template

---
+++ BookingApp/src/Controllers/mainController.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
+
+public class mainController {
+
+    @FXML
+    private TextField bookingNameField;
+
+    @FXML
+    private DatePicker bookingTimeField;
+
+    @FXML
+    private TextField bookingFromField;
+
+    @FXML
+    private TextField bookingToField;
+
+    @FXML
+    private WebView mapView;
+
+}
