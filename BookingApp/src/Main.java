3525e9fabda7a09a36f10170cfe84f55279f1e00
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-04-27 02:04:00 +0100

    commit of main - added relevant FXML tags and initialised maps. Added address lookup function for to field of address

--- BookingApp/src/Main.java
+++ BookingApp/src/Main.java
@@ -7,14 +7,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
     public static void main(String[] args) {
-        //launch(args);
-        System.out.println("Hello");
+        launch(args);
     }
 
     @Override
     public void start(Stage window) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainLayout.fxml"));
         window.setTitle("Desktop Booking & Dispatch System");
+        window.setMinWidth(1024);
+        window.setMinHeight(720);
         window.setScene(new Scene(root, 800, 600));
         window.show();
     }
