797903b683365dd488c4cc91749c307a703eaecc
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-04-28 10:32:01 +0100

    Added parsing method to get journey time.

--- BookingApp/src/Utils/XmlParser.java
+++ BookingApp/src/Utils/XmlParser.java
@@ -7,34 +7,50 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Scanner;
 
 public class XmlParser {
     public static void main(String[] args) throws ParserConfigurationException, TransformerException, SAXException, IOException {
         XmlParser x = new XmlParser();
-        Scanner s = new Scanner(System.in);
-        String t = s.next();
-        System.out.println(x.addressFromPostCode(t));
+//        Scanner s = new Scanner(System.in);
+//        String t = s.next();
+//        System.out.println(x.addressFromPostCode(t));
+        System.out.println("Time: "+x.getJourneyDuration("n166lh","e58au"));
     }
     public String addressFromPostCode (String postcode) throws IOException, ParserConfigurationException, SAXException, TransformerException {
         URL url = new URL("https://maps.googleapis.com/maps/api/geocode/xml?address="+postcode+"&key=AIzaSyB2kViW5AL4Q1vGaik53tkw6Ve1ZXdT2vg");
-        URLConnection conn = url.openConnection();
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(conn.getInputStream());
-        TransformerFactory tfactory = TransformerFactory.newInstance();
-        Transformer xform = tfactory.newTransformer();
-        NodeList movieList = doc.getElementsByTagName("formatted_address");
+        Document doc = generateDoc(url);
+        NodeList addressList = doc.getElementsByTagName("formatted_address");
         try{
-            return movieList.item(0).getFirstChild().getTextContent();
+            return addressList.item(0).getFirstChild().getTextContent();
         }catch (NullPointerException e){
             return postcode.toUpperCase();
         }
     }
+
+    public String getJourneyDuration (String origin, String destination) throws IOException, ParserConfigurationException, SAXException, TransformerException {
+        origin = origin.replaceAll("\\s", "+");
+        destination = destination.replaceAll("\\s", "+");
+        URL url = new URL("https://maps.googleapis.com/maps/api/directions/xml?origin="+origin+"&destination="+destination+"&key=AIzaSyB2kViW5AL4Q1vGaik53tkw6Ve1ZXdT2vg");
+        Document doc = generateDoc(url);
+        NodeList journeyList = doc.getElementsByTagName("text");
+        try{
+            return journeyList.item(journeyList.getLength()-2).getFirstChild().getTextContent();
+        }catch (NullPointerException e){
+            return "Not Calculated";
+        }catch (ArrayIndexOutOfBoundsException e){
+            return "Not Calculated";
+        }
+    }
+
+    private Document generateDoc(URL url) throws IOException, ParserConfigurationException, SAXException {
+        URLConnection conn = url.openConnection();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return  builder.parse(conn.getInputStream());
+    }
+
 }
