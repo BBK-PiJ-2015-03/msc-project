55d0814de1fe485e4911f281aadc4c42cf66b5b1
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-05-01 18:55:26 +0100

    Testing time validator class

---
+++ test/TimeValidatorTest.java
@@ -0,0 +1,30 @@
+/**
+ * Testing for TimeFieldValidator
+ * Majority of test names will be self explanatory, if need be I will comment.
+ */
+
+import org.junit.Test;
+import resources.TimeFieldValidator;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TimeValidatorTest {
+    private TimeFieldValidator validator = new TimeFieldValidator();
+
+
+    @Test
+    public void test1000RandomTimes() {
+        // Testing validator & format method of TimeFieldValidator
+        for(int i=0; i <= 1000; i++) {
+            String hour = ((int) (24 * Math.random())) + "";
+            String minutes = ((int) (60 * Math.random())) + "";
+            if(minutes.length() == 1){
+                minutes = "0"+minutes;
+            }
+            assertTrue(validator.validate(validator.format(hour + ":" + minutes)));
+        }
+
+    }
+
+
+}
