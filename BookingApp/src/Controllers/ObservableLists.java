ab597096dd6e7b288dd23f4cdab6dd9a11456160
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-05-02 02:30:57 +0100

    Added another observable list for uncompleted jobs

---
+++ BookingApp/src/Controllers/ObservableLists.java
@@ -0,0 +1,31 @@
+package Controllers;
+
+import Model.Archive;
+import Model.BookingImpl;
+import Model.Cash;
+import Model.Interfaces.Account;
+import Model.Interfaces.Driver;
+import Model.VehicleTypes;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Class to hold all observable lists displayed in the GUI
+ */
+public class ObservableLists {
+    public static ObservableList<Account> accountList = FXCollections.observableArrayList();
+    public static ObservableList<Driver> driverList = FXCollections.observableArrayList();
+    public static ObservableList<String> vehicleTypeList = FXCollections.observableArrayList();
+    public static ObservableList<BookingImpl> bookingsList = FXCollections.observableArrayList();
+
+    /**
+     * Populates the observable lists.
+     */
+    public static void initialise(){
+        accountList.add(Cash.getInstance());
+        accountList.addAll(Archive.allAccounts);
+        driverList.addAll(Archive.allDrivers);
+        vehicleTypeList.addAll(VehicleTypes.types);
+        bookingsList.addAll(Archive.incompleteBookings);
+    }
+}
