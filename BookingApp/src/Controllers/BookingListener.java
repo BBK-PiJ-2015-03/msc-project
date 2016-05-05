dd24172b02ec7c3b4ec76ca4c494c7afbd16e318
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-05-05 05:26:51 +0100

    Booking number is now in sync with online database, therefore if someone adds a booking on one instance of the application, someone using another desktop running a seperate instance will see the booking number being updated instantly to avoid overwritting bookings

--- BookingApp/src/Controllers/BookingListener.java
+++ BookingApp/src/Controllers/BookingListener.java
@@ -1,11 +1,14 @@
 package Controllers;
 
 import Model.BookingImpl;
+import Model.Cash;
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import javafx.application.Platform;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created by J2FX on 04/05/2016.
@@ -17,49 +20,79 @@ public class BookingListener {
         bRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                dataSnapshot.getChildren().forEach(c -> System.out.println((c) +"\n Child Added"));
                 Platform.runLater(() -> {
                     LoadOnlineDatabase load = new LoadOnlineDatabase();
                     load.loadBookings();
+                    MainController m = new MainController();
+                    m.refreshBookingNo();
                 });
+                // Must run on FX thread - otherwise throws exception but still functions well either way ? weird
+
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                 System.out.println(s);
                 System.out.println("Change happened!");
+                //Have to work out what change happened and apply it to local object
 
             }
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
                 System.out.println("Remove happened!");
+                // delete local object of child removed
 
 
             }
 
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
+                //Do nothing
             }
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-
+                //If internet connection cut - maybe wait for 1 minute and try again?
             }
         });
     }
 
     public static void addBooking(BookingImpl b){
         Firebase bRefChild = bRef.child(b.getBookingNumber()+"");
-        bRefChild.child("pickup").setValue(b.getPickUpAddress());
-        bRefChild.child("dropoff").setValue(b.getDropOffAddress());
-        bRefChild.child("name").setValue(b.getClientName());
+        System.out.println(1);
+        bRefChild.child("pickup").setValue(b.getPickUpAddress());//
+        System.out.println(2);
+        bRefChild.child("dropoff").setValue(b.getDropOffAddress());//
+        System.out.println(3);
+        bRefChild.child("name").setValue(b.getClientName());//
+        System.out.println(4);
         bRefChild.child("time").setValue(b.getTime().toString());
-        bRefChild.child("comment").setValue(b.getComments());
-        bRefChild.child("tel").setValue(b.getClientTel());
-        bRefChild.child("email").setValue(b.getClientEmail());
-        bRefChild.child("booking_number").setValue(b.getBookingNumber());
-//        bRef.child("date").setValue(b.getDate());
+        System.out.println(5);
+        bRefChild.child("comment").setValue(b.getComments());//
+        System.out.println(6);
+        bRefChild.child("tel").setValue(b.getClientTel());//
+        System.out.println(7);
+        bRefChild.child("email").setValue(b.getClientEmail());//
+        System.out.println(8);
+        bRefChild.child("booking_number").setValue(b.getBookingNumber());//
+        System.out.println(9);
+        bRefChild.child("account").setValue(Cash.getInstance().getId());
+        System.out.println(10);
+        bRefChild.child("vehicle_type").setValue(b.getVehicleType());
+        System.out.println(11);
+        bRefChild.child("no_passengers").setValue(b.getNoPassengers());
+        System.out.println(12);
+        bRefChild.child("date").setValue(b.getDate());
+        System.out.println(13);
+        try {
+            bRefChild.child("time").setValue(new SimpleDateFormat("HH:mm").format(b.getTime()));
+        } catch (IllegalArgumentException e){
+            // Always throws this exception - unsure why as it saves correctly.
+            // Do nothing as it still writes to database as expected
+        }
+        System.out.println(14);
+        bRefChild.child("price").setValue(b.getPrice());
+        System.out.println(15);
     }
 }
